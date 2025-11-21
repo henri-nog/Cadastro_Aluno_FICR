6fe020055c5d7cdda0c7c55caf2509c6f391fdde
Fellipe Henrique <fellipe.caluete@a.ficr.edu.br>
2025-11-21 12:07:33 -0300

    Incluindo os campos essenciais da aplicação
    Added new fields 'id_usuario', 'email', 'idade', and 'curso' to the Produto model.

--- produtos/models.py
+++ produtos/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Produto(models.Model):
+    id_usuario = models.AutoField(primary_key=True) 
     nome = models.CharField(max_length=100)
-    quantidade = models.IntegerField()
-    preco = models.DecimalField(max_digits=10, decimal_places=2)
-    data_validade = models.DateTimeField()
+    email = models.EmailField()
+    idade = models.IntegerField()
+    curso = models.CharField(max_length=100)
     data_cadastro = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
