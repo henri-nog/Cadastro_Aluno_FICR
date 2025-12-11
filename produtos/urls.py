2760d5218d6698f911ca30fd8f318d57844d7f8d
Aluno <Aluno@151ET1109560.catolica.ubec>
2025-12-11 14:51:57 -0300

    Alterando a rota inicial para 'home'

--- produtos/urls.py
+++ produtos/urls.py
@@ -3,23 +3,23 @@ from . import views
 
 urlpatterns = [
 
-    # HOME
-    path("home/", views.home, name="home"),
+    # home 
+    path("", views.home, name="home"),
 
-    # PRODUTOS
-    path("", views.listar_produto, name="listar_produto"),
-    path("adicionar/", views.adicionar_produto, name="adicionar_produto"),
-    path("editar/<int:id>/", views.editar_produto, name="editar_produto"),
-    path("excluir/<int:id>/", views.excluir_produto, name="excluir_produto"),
-    path("produto/<int:id>/", views.detalhes_produto, name="detalhes_produto"),
+    # produtos
+    path("produtos/", views.listar_produto, name="listar_produto"),
+    path("produtos/adicionar/", views.adicionar_produto, name="adicionar_produto"),
+    path("produtos/editar/<int:id>/", views.editar_produto, name="editar_produto"),
+    path("produtos/excluir/<int:id>/", views.excluir_produto, name="excluir_produto"),
+    path("produtos/<int:id>/", views.detalhes_produto, name="detalhes_produto"),
 
-    # CATEGORIAS
+    # categorias
     path("categorias/", views.listar_categorias, name="listar_categorias"),
     path("categorias/adicionar/", views.adicionar_categoria, name="adicionar_categoria"),
     path("categorias/editar/<int:id>/", views.editar_categoria, name="editar_categoria"),
     path("categorias/excluir/<int:id>/", views.excluir_categoria, name="excluir_categoria"),
 
-    # PEDIDOS
+    # pedidos
     path("pedidos/", views.listar_pedidos, name="listar_pedidos"),
     path("pedidos/adicionar/", views.adicionar_pedido, name="adicionar_pedido"),
     path("pedidos/editar/<int:id>/", views.editar_pedido, name="editar_pedido"),
